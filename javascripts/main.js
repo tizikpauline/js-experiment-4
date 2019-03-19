369a23143ce9edaba60b630c3f4e5b3f21e2f818
Tizik Pauline <43169531+tizikpauline@users.noreply.github.com>
2019-03-19 14:26:49 +0300

    changed images width

--- javascripts/main.js
+++ javascripts/main.js
@@ -156,7 +156,7 @@ class Menu {
         // Translate the image on mousemove
         this.lastMousePos.translation.x = lerp(this.lastMousePos.translation.x, this.mousePos.x, 0.2);
         this.lastMousePos.translation.y = lerp(this.lastMousePos.translation.y, this.mousePos.y, 0.2);
-        this.DOM.svg.style.transform = `translateX(${(this.lastMousePos.translation.x-winsize.width/2)}px) translateY(${this.lastMousePos.translation.y-winsize.height*1.5}px)`;
+        this.DOM.svg.style.transform = `translateX(${(this.lastMousePos.translation.x-winsize.width/2)}px) translateY(${this.lastMousePos.translation.y-winsize.height*1.4}px)`;
 
         // Scale goes from 0 to 50 for mouseDistance values between 0 to 140
         this.lastMousePos.displacement.x = lerp(this.lastMousePos.displacement.x, this.mousePos.x, 0.1);
